cdb7a56094dfe66c2621075ffee5a1e592caba5b
Quigiaosu103 <52100996@student.tdtu.edu.vn>
2024-11-20 03:37:52 +0700

    :rocket: feat:  update sui wallet, transfer, result

--- src/components/TransferForm/index.tsx
+++ src/components/TransferForm/index.tsx
@@ -1,11 +1,42 @@
 'use client'
 import { useState } from "react";
-
+import { useCurrentAccount, useCurrentWallet, useDisconnectWallet, useSignTransaction, useSuiClient } from "@mysten/dapp-kit";
+import { getFullnodeUrl, SuiClient } from '@mysten/sui.js/client';
+import { Transaction } from '@mysten/sui/transactions';
+const rpcUrl = getFullnodeUrl('devnet');
+ import { useWallets } from "@mysten/dapp-kit";
+import { TransactionBlock } from "@mysten/sui.js/transactions";
+import { toast } from "react-toastify";
+// create a client connected to devnet
+const client = new SuiClient({ url: rpcUrl });
 function TransferForm() {
+    const network = 'devnet';
+    console.log();
     const [address, setAddress] = useState<string>("");
     const [amount, setAmount] = useState<number>(0);
     const tokenSymbol = 'SUI';
-    return ( <div className="w-fit py-[48px] p-[48px] rounded-[4px] border-[1px] flex-col border-secondary shadow-md flex items-center justify-center">
+    const { mutateAsync: signTransaction } = useSignTransaction();
+    
+
+    const h = async () => {
+        const tx = new Transaction();
+        tx.setGasBudget(2000000);
+        const [coin] = tx.splitCoins(tx.gas, [amount]);
+        tx.transferObjects([coin], address); 
+        const { bytes, signature, reportTransactionEffects } = await signTransaction({
+            transaction: tx,
+            chain: 'sui:devnet',
+        });
+        const executeResult = await client.executeTransactionBlock({
+            transactionBlock: bytes,
+            signature
+        });
+        toast.success(<span className="font-[500] text-[13px]"> Transaction Successfully! <a onClick={() => open(`https://${network}.suivision.xyz/txblock/${executeResult.digest}`)} className="underline text-secondary cursor-pointer">View Transaction</ a></span>);
+    }
+
+   
+    return ( 
+    <div className="w-fit py-[48px] p-[48px] rounded-[4px] border-[1px] flex-col border-secondary shadow-md flex items-center justify-center">
         <span className=" block text-secondary text-[24px] font-[600] mb-4 ">Transfer Token</span>
         <input type="text" value={address} onChange={(e) => setAddress(e.target.value)}  className="w-[320px] bg-[#e4d9fa] py-2 px-3 rounded-[4px] text-black focus:bg-[#cfb8ff] my-6 active:outline-none focus:outline-none" placeholder="Receiver Address" />
         
@@ -21,7 +52,7 @@ function TransferForm() {
         
         <button onClick={() => {
             if (!address || amount == 0) return;
-            console.log({address, amount});
+            h();
         }} className=" mt-[64px] mb-10  w-[320px] before:content-[''] before:absolute relative before:left-[-54px] before:block overflow-hidden before:top-[-40px] before:w-8 before:h-[200px] before:bg-[#ffffff61] before:rotate-45 bg-gradient-to-r from-primary to-secondary py-2 px-3 rounded-[4px] text-white my-6 active:outline-none focus:outline-none hover:before:translate-x-[320px] before:transition-all before:ease-in-out before:duration-300 active:scale-[.99]">Transfer</button>
 
     </div> );
--- src/layout/components/Heading/index.tsx
+++ src/layout/components/Heading/index.tsx
@@ -1,8 +1,10 @@
 'use client'
 import { DataContext, useDataContext } from "@/provider/DataContext";
 import HeadignButton from "../../../components/HeadingButton";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { Coins } from "lucide-react";
+import { ConnectButton, useAccounts, useCurrentWallet } from '@mysten/dapp-kit';
+import { SuiClient, getFullnodeUrl } from "@mysten/sui.js/client";
 const pages = [
     {
         title: 'Home',
@@ -18,10 +20,28 @@ const pages = [
     }
 ]
 function Heading() {
+    const { currentWallet, connectionStatus } = useCurrentWallet();
+    const accounts = useAccounts();
+    const [balance, setBalance] = useState<any>(null);
     const context =useContext(DataContext)
     if(!context) {
         throw new Error('useDataContext must be used within a DataProvider');
     }
+
+
+
+    const rpcUrl = getFullnodeUrl('devnet');
+// create a client connected to devnet
+        const client = new SuiClient({ url: rpcUrl });
+        useEffect(() => {
+            if(connectionStatus == 'connected') {
+                client.getCoins({
+                    owner: accounts[0].address,
+                }).then(res => setBalance(res.data.length > 0? res.data.filter((coin: any) => coin.coinType == '0x2::sui::SUI')[0].balance:0));
+        }}
+        , [connectionStatus])
+    
+    console.log(accounts)
     const {activePage, setActivePage} =context;
     return ( <div className="w-full pt-6 px-[100px] flex items-center justify-end">
         {
@@ -30,9 +50,12 @@ function Heading() {
             }} />  )  
 
         }
-        <div className="flex ml-6 items-center justify-center"><Coins size={14} weight={1} strokeWidth={3}  /><span className="ml-2 text-[14px] font-[600]">99.999</span></div>
-        <button className="ml-8"> <span className="text-white bg-secondary px-2 py-1 rounded-[4px] hover:shadow-sm hover:shadow-secondary text-[16px] font-[600]">Connect Wallet</span>
-        </button>
+        {connectionStatus == 'connected'&&<><div className="flex ml-6 items-center justify-center "><Coins size={14} strokeWidth={3}  />
+            <span className="ml-2 text-[14px] font-[600]">{balance}</span>
+        </div>
+        <button className="ml-8"> <span className="text-white bg-secondary px-2 py-1 rounded-[4px] hover:shadow-sm hover:shadow-secondary text-[16px] font-[600]">{accounts[0]?.address}</span>
+        </button></>}
+        <div className="ml-4"><ConnectButton  /></div>
     </div> );
 }
 
